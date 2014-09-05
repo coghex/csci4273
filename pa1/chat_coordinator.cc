f6beab9b2cc3a08419fb811146a5f6f70c72c5be
Vincent Coghlan <fatty@engr2-5-19-dhcp.int.colorado.edu>
2014-09-05 12:42:59 -0600

    works much better now

--- pa1/chat_coordinator.cc
+++ pa1/chat_coordinator.cc
@@ -7,30 +7,83 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+//the size of all my buffers cause im lazy
 #define BUFSIZE 2048
 
+//the nodes in the list that will hold all of the servers
+struct server {
+  int port;
+  struct server* next;
+  char* name;
+};
 
+//this server is not real, but the list head
+struct server* head;
+
+//finds if the server already exists and returns the port if it does
 int existingsession(char * input) {
-  return 1;
+  struct server *node = head;
+  while(node->next!=NULL) {
+    if (!strcmp(node->name, input)) {
+      return node->port;
+    }
+    node = node->next;
+  }
+  if (!strcmp(node->name, input)) {
+    return node->port;
+  }
+
+  return 0;
+}
+
+//mallocs and adds the server to the list
+void addserver(char * name, int port) {
+  struct server *node = head;
+  while(node->next!=NULL) {
+    node=node->next;
+  }
+  node->next = (struct server*)malloc(sizeof(struct server));
+  node=node->next;
+  node->port = port;
+  node->name = (char *)malloc(BUFSIZE);
+  strcpy(node->name, name);
+}
+
+//removes a server from the list and rearranges the list
+void termsession(int p) {
+  struct server *node = head;
+  while(node->next->port != p) {
+    node=node->next;
+  }
+  if (node->next->next != NULL) {
+    node->next = node->next->next;
+  }
+  else {
+    node->next = NULL;
+  }
 }
 
 int main (int argc, char *argv[0]) {
   unsigned short port;
   struct sockaddr_in saddr, caddr, tcpsaddr, tcpcaddr;
   int sockfd, pid, tcpsockfd, tcpconnfd;
-  char* buf, *cmd, *arg;
-  int ret, i, j;
+  char* buf, *cmd, *arg, *sendbuf;
+  int ret, i, j, p;
   int tcpport=31337;
   socklen_t slen, clen, tcpslen, tcpclen;
 
+  //first i allocate all of the buffers
+  //all of this is super hacky c code because yolo
   buf = (char*)malloc(BUFSIZE);
   memset(buf, 0, BUFSIZE);
   cmd = (char*)malloc(BUFSIZE);
   memset(cmd, 0, BUFSIZE);
   arg = (char*)malloc(BUFSIZE);
   memset(arg, 0, BUFSIZE);
+  sendbuf = (char*)malloc(BUFSIZE);
+  memset(sendbuf, 0, BUFSIZE);
 
-
+  //we want to make sure the user specifies a port
   if (argc != 2) {
     printf("usage: %s <Port>\n", argv[0]);
     exit(1);
@@ -38,66 +91,129 @@ int main (int argc, char *argv[0]) {
 
   port = atoi(argv[1]);
 
+  //if the port they specified is the default tcp starting port, we
+  //change that port by one, nbd
+  if (port == 31337) {
+    tcpport = 31338;
+  }
+
+  //initialize the head of the list with dummy values
+  //so we dont get null pointer exceptions
+  head = (struct server*)malloc(sizeof(struct server));
+  head->port = -1;
+  head->name = (char *)malloc(BUFSIZE);
+  strcpy(head->name, "head1337\n");
+
+  //opens the udp socket
   if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    printf("cannot open socket\n");
+    printf("cannot open udp socket\n");
     exit(1);
   }
+  //initializes the address structures
   memset((char*)&saddr, 0, sizeof(saddr));
   memset((char*)&caddr, 0, sizeof(caddr));
   saddr.sin_family = AF_INET;
   saddr.sin_addr.s_addr = htonl(INADDR_ANY);
   saddr.sin_port = htons(port);
 
+  //binds to the udp socket
   if (bind(sockfd, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
     printf("cannot bind\n");
     exit(1);
   }
   printf("The chat coordinator is now on port %d\n", port);
+  //this is our infinite loop
   while (1) {
     clen = sizeof(caddr);
-    ret = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&caddr, &clen);
+    //block until we recv something, then find out where it came from
+    if ((ret = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&caddr, &clen)) < 0) {
+      sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+      goto cleanup;
+    }
+    //seperate the arguments from the supplied buffer in a super hacky way
     for(i=0;(buf[i]!=0x20)&&(buf[i]!=0x00)&&(buf[i]!='\n');i++);
     cmd = (char*)memcpy(cmd, buf, i);
     for(j=i+1;(buf[j]!=0x20)&&(buf[j]!=0x00)&&(buf[j]!='\n');j++);
     arg = (char*)memcpy(arg, buf+i+1, j-i);
 
-    if (!strcmp(cmd, "Start")) {
+    //if we receive a start command
+    if (!strcmp(cmd, "Start") || !strcmp(cmd, "s")) {
+      //does the session already exist?
       if (existingsession(arg)) {
-        sendto(sockfd, "-1", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
-        close(sockfd);
-        exit(1);
+        printf("a server by the name %s is already running\n", arg);
+        sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        goto cleanup;
       }
+      //open the tcp socket
       if ((tcpsockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        sendto(sockfd, "-1", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
-        close(sockfd);
-        exit(1);
+        printf("cannot open tcp socket\n");
+        sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        goto cleanup;
       }
+      //add this server to the list, since at this point, it will probably be made
+      addserver(arg, tcpport);
       memset((char*)&tcpsaddr, 0, sizeof(tcpsaddr));
       memset((char*)&tcpcaddr, 0, sizeof(tcpcaddr));
       tcpsaddr.sin_family = AF_INET;
       tcpsaddr.sin_addr.s_addr = htonl(INADDR_ANY);
       tcpsaddr.sin_port = htons(tcpport);
+      //bind to the tcp socket, if there is an error, remove the session from the
+      //list, not sure if that works, but im gonna guess it does
       if (bind(tcpsockfd, (struct sockaddr*)&tcpsaddr, sizeof(tcpsaddr)) < 0) {
-        sendto(sockfd, "-1", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
-        exit(1);
+        printf("cannot bind to port %d\n", tcpport);
+        sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        termsession(p);
+        close(tcpport);
+        goto cleanup;
       }
+      //accept on that port
       tcpclen = sizeof(tcpcaddr);
       tcpconnfd = accept(tcpsockfd, (struct sockaddr*)&tcpcaddr, &tcpclen);
+      printf("starting server %s on port %d\n", arg, tcpport);
+      //fork a new session to do whatevs
       if (!(pid = fork())) {
-        close(tcpsockfd);
-        sendto(sockfd, "success", 8, 0, (struct sockaddr *)&caddr, sizeof(caddr));
-        exit(0);
+        snprintf(sendbuf, BUFSIZE, "The Chat server is now running on port %d\n", tcpport);
+        sendto(sockfd, sendbuf, BUFSIZE, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        close(sockfd);
+        while(1);
       }
       else {
         close(tcpconnfd);
-        while((pid = waitpid(((pid_t) - 1), NULL, WNOHANG)) > 0);
+        //increment the tcp port by one, it would be tough to run out
+        tcpport++;
       }
     }
-    else {
-      memset(buf, 0, BUFSIZE);
-      memset(cmd, 0, BUFSIZE);
-      memset(arg, 0, BUFSIZE);
+    //on find we return the port, simple enough
+    if (!strcmp(cmd, "Find") || !strcmp(cmd, "f")) {
+      if ((p = existingsession(arg))) {
+        snprintf(sendbuf, BUFSIZE, "%d\n", p);
+        sendto(sockfd, sendbuf, BUFSIZE, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        goto cleanup;
+      }
+      else {
+        printf("a server by the given name is not running\n");
+        sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        goto cleanup;
+      }
     }
+    //terminate calls the termsession code, its pretty straitforward
+    if (!strcmp(cmd, "Terminate") || !strcmp(cmd, "t")) {
+      if ((p = existingsession(arg))) {
+        close(tcpport);
+        termsession(p);
+      }
+      else {
+        printf("a server by the given name is not running\n");
+        sendto(sockfd, "-1\n", 3, 0, (struct sockaddr *)&caddr, sizeof(caddr));
+        goto cleanup;
+      }
+
+    }
+    //what would hacky c code be without gotos, labels, and memsets
+    cleanup:
+    memset(buf, 0, BUFSIZE);
+    memset(cmd, 0, BUFSIZE);
+    memset(arg, 0, BUFSIZE);
   }
 return 0;
 }
